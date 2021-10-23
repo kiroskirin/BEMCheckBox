cb54b9a3b27d3d63db12d229f3219009e954edba
kraisorn.soi <kraisorn.soi@ascendcorp.com>
2021-10-23 19:23:49 +0700

    :zap: - update package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "BEMCheckBox",
     platforms: [
-       .iOS(.v8)
+        .iOS(.v10)
     ],
     products: [
         .library(name: "BEMCheckBox", targets: ["BEMCheckBox"])
